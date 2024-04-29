144aaef82d8de7c9317f71396410422258a79f5f
Honghyesoo <ghdgptn12@naver.com>
2024-04-29 09:39:30 +0900

    story.html이미지만 변경

--- js/common.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $(window).scroll(function () {
-    var value = $(this).scrollTop();
-
-    if (value > 300) {
-      $(".p_chapSub").animation({ left: 0, opacity: 1 }, 1000);
-    }
-  });
-});
